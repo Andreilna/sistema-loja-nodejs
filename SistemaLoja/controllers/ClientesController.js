8661f6c2305c304f9af5dd2bfd227258bf018731
Andrei <andreialda7@gmail.com>
2025-04-16 11:47:14 -0300

    @Andreilna

--- SistemaLoja/controllers/ClientesController.js
+++ SistemaLoja/controllers/ClientesController.js
@@ -3,9 +3,9 @@ import express from "express";
 const router = express.Router();
 
 const clientes = [
-  { nome: "Érlon Viana", depoimento: "Comprei o Nada e chegou tudo! Incrível, realmente não tem igual!", imagem: "/img/depoimento1.jpg" },
-  { nome: "Ricardo Estevam", depoimento: "Produto invisível e de qualidade duvidosa! Recomendo muito, mas não sei o quê!", imagem: "/img/depoimento2.jpg" },
-  { nome: "João Vitor", depoimento: "Eu comprei o 'Silêncio Absoluto' e agora não consigo ouvir nada. Mas é um silêncio maravilhoso!", imagem: "/img/depoimento3.jpg" },
+  { nome: "Érlon Viana", depoimento: "Comprei o Nada e chegou tudo! Incrível, realmente não tem igual!", imagem: "/img/user.png" },
+  { nome: "Ricardo Estevam", depoimento: "Produto invisível e de qualidade duvidosa! Recomendo muito, mas não sei o quê!", imagem: "/img/user.png" },
+  { nome: "João Vitor", depoimento: "Eu comprei o 'Silêncio Absoluto' e agora não consigo ouvir nada. Mas é um silêncio maravilhoso!", imagem: "/img/user.png" },
 ];
 
 router.get("/clientes", (req, res) => {
--- SistemaLoja/controllers/ProdutosController.js
+++ SistemaLoja/controllers/ProdutosController.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 router.get("/produtos", (req, res) => {
   const produtos = [
-    { nome: "Ar Engarrafado Premium", preco: "Indisponível", descricao: "Respire estilo. Não contém oxigênio.", imgSrc: "/img/nada1.png" },
-    { nome: "Nada em Pó", preco: "Indisponível", descricao: "Ideal pra quem já se cansou de tudo.", imgSrc: "/img/nada2.png" },
-    { nome: "Botão que Não Faz Nada", preco: "Indisponível", descricao: "Clique à vontade. Ele ignora você.", imgSrc: "/img/nada3.png" },
-    { nome: "Nada de Valor", preco: "Indisponível", descricao: "Pague por algo que não vale nada.", imgSrc: "/img/nada4.png" },
-    { nome: "Espaço Vazio", preco: "Indisponível", descricao: "Apenas um buraco no espaço-tempo.", imgSrc: "/img/nada5.png" },
-    { nome: "Silêncio Absoluto", preco: "Indisponível", descricao: "A experiência do nada em sua forma mais pura.", imgSrc: "/img/nada6.png" }
+    { nome: "Ar Engarrafado Premium", preco: "Indisponível", descricao: "Respire estilo. Não contém oxigênio.", imgSrc: "/img/air.png" },
+    { nome: "Nada em Pó", preco: "Indisponível", descricao: "Ideal pra quem já se cansou de tudo.", imgSrc: "/img/nothing.png" },
+    { nome: "Botão que Não Faz Nada", preco: "Indisponível", descricao: "Clique à vontade. Ele ignora você.", imgSrc: "/img/button.png" },
+    { nome: "Nada de Valor", preco: "Indisponível", descricao: "Pague por algo que não vale nada.", imgSrc: "/img/spoon.png" },
+    { nome: "Espaço Vazio", preco: "Indisponível", descricao: "Apenas um buraco no espaço-tempo.", imgSrc: "/img/empty.png" },
+    { nome: "Silêncio Absoluto", preco: "Indisponível", descricao: "A experiência do nada em sua forma mais pura.", imgSrc: "/img/silence.png" }
   ];
 
   res.render("produtos", { produtos: produtos });
